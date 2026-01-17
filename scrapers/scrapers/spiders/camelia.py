adefcfff5cbbf26235ea4257707582d2eca7ce3d
Arnoldas Ambrasas <ambrasas.arnoldas@gmail.com>
2026-01-17 19:02:45 +0200

    Added next page and increased delay in settings.py for safety during testing phase

--- scrapers/scrapers/spiders/camelia.py
+++ scrapers/scrapers/spiders/camelia.py
@@ -15,6 +15,15 @@ class CameliaSpider(scrapy.Spider):
                 'price': product.css('div.price::text').get(),
             }
 
+        current_page = response.meta.get("page", 1)
+        next_page = current_page + 1
 
+        if next_page <= 121:
+            next_url = f"https://camelia.lt/akcijos?page={next_page}"
+            yield response.follow(
+                next_url,
+                callback=self.parse,
+                meta={"page": next_page}
+            )
 
 
