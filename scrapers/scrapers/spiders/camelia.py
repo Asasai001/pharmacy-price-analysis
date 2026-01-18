76207797c8bac209dbba3b5c6290051402b59ddb
Arnoldas Ambrasas <ambrasas.arnoldas@gmail.com>
2026-01-18 18:33:20 +0200

    Adding scraping logic to every product separately

--- scrapers/scrapers/spiders/camelia.py
+++ scrapers/scrapers/spiders/camelia.py
@@ -6,18 +6,7 @@ class CameliaSpider(scrapy.Spider):
     allowed_domains = ["camelia.lt"]
     start_urls = ["https://camelia.lt/akcijos"]
 
-    #Current price with discount or without discount if there are conditions
-    base_price = product.css('div[data-test="product-price"] div[data-test="product-price-formatted"]::text').get()
 
-    #Price before discount (no conditions)
-    old_price = product.css('span[data-test^="product-price-original-item"]::text').get()
-
-    #Price with discount if conditions are applied
-    conditional_discount_price = product.css('span.discounted-price-value::text').get()
-
-    #Conditions of the discount
-    discount_condition = product.css('div.badge-content div::text').getall()
-    discount_condition = " ".join(t.strip() for t in discount_condition if t.strip())
 
     if conditional_discount_price:
         discount_price = conditional_discount_price
@@ -51,15 +40,31 @@ class CameliaSpider(scrapy.Spider):
                 meta={"page": next_page},
             )
 
-
     def parse_product_page(self, response):
         product = response.css("div.product-grid")
+
+        # Current price with discount or without discount if there are conditions
+        base_price = product.css('div[data-test="product-price"] div[data-test="product-price-formatted"]::text').get()
+
+        # Price before discount (no conditions)
+        old_price = product.css('span[data-test^="product-price-original-item"]::text').get()
+
+        # Price with discount if conditions are applied
+        conditional_discount_price = product.css('span.discounted-price-value::text').get()
+
+        # Conditions of the discount
+        discount_condition = product.css('div.badge-content div::text').getall()
+        discount_condition = " ".join(t.strip() for t in discount_condition if t.strip())
+
+        company_name = product.css('a[href^="/a/prekes-zenklas/"]::text').get()
 
         yield {
             'name': product.css('div[data-test^="product-name"]::text').get(),
             'product_code':  product.css('div[data-test^="product-code"]::text').get(),
-            ''
+
         }
+
+        category = product.css('div.product-additional-info a::text').get()
 
 
 
