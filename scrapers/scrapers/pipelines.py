5529378ad5c2c567bf960f0657f202b90683f6ff
Arnoldas Ambrasas <ambrasas.arnoldas@gmail.com>
2026-01-19 12:43:53 +0200

    added pipelines cleaning: strip, lower and converting to float

--- scrapers/scrapers/pipelines.py
+++ scrapers/scrapers/pipelines.py
@@ -10,4 +10,24 @@ from itemadapter import ItemAdapter
 
 class ScrapersPipeline:
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+        field_names = adapter.field_names()
+        for field_name in field_names:
+          value = adapter.get(field_name)
+          if isinstance(value, str):
+            value = value.strip().lower()
+            value = value.replace('\xa0€', '')
+            adapter[field_name] = value
+
+        price_keys = ['base_price', 'old_price', 'conditional_discount_price', 'final_price']
+        for price_key in price_keys:
+          value = adapter.get(price_key)
+          if value in (None, ''):
+            adapter[price_key] = None
+          else:
+            value = value.replace(',', '.')
+            adapter[price_key] = float(value)
+
+
         return item
